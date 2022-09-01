0249c63a68086a8d43835cbbd63db901e8959186
pdolder <paul.dolder@gmail.com>
2022-09-01 13:00:49 +0100

    Remove renaming of dimnames

--- R/mizer_functions.R
+++ R/mizer_functions.R
@@ -1004,7 +1004,7 @@ get_transfer <- function(mod, prop_yr = 0.5){
   weight_end_of_year <- weight_at_age[floor(as.numeric(dimnames(num_at_age)[[1]]))+prop_yr==(as.numeric(dimnames(num_at_age)[[1]])),,]
  
   ## Remove the year-fraction identifier
-  dimnames(end_of_year)[[1]] <- dimnames(end_of_year_m)[[1]] <- dimnames(weight_end_of_year)[[1]] <- as.character(floor(as.numeric(dimnames(end_of_year)[[1]])))
+#  dimnames(end_of_year)[[1]] <- dimnames(end_of_year_m)[[1]] <- dimnames(weight_end_of_year)[[1]] <- as.character(floor(as.numeric(dimnames(end_of_year)[[1]])))
 
   if(prop_yr==0) {  
   return(list(num_at_age=end_of_year[-1,,],Ms=break_mort$Ms[-1,,],Fs=break_mort$Fs[-1,,],cat_bio_at_age=an_cat_bio_aa[-1,,],cat_num_at_age=an_cat_num_aa[-1,,],mean_cwaa=an_cat_bio_aa[-1,,]/an_cat_num_aa[-1,,],prop_mat=end_of_year_m[-1,,]/end_of_year[-1,,],mean_swaa = weight_end_of_year[-1,,]/end_of_year[-1,,]))}
